6cfe768440d42e84cab38840c64f46c2407bd727
sarveenyadav7 <sarveenyadav7@gmail.com>
2023-08-21 09:38:48 +0530

    home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import 'font-awesome/css/font-awesome.min.css'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
