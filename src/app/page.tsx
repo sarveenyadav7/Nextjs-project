6a87fedd3074f67fc96864aa8e5a659201344608
sarveenyadav7 <sarveenyadav7@gmail.com>
2023-08-21 09:43:02 +0530

    check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
                 industry. Lorem Ipsum has been the industry's standard dummy
                 text ever since the 1500s, when an unknown printer took a galley
                 of type and scrambled it to make a type specimen
-                book..................................
+                book
               </p>
             </div>
           </div>
